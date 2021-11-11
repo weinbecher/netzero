ea830c275ea61c49ef6491e07c1bdc9f297c10a5
weinbecher <382316736@qq.com>
2021-11-11 20:02:16 +0000

    add container

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import MapComponent from "../components/MapComponent"
+
+export default function HomeContainer(){
+    return(
+        <div>
+        <MapComponent/>
+        </div>
+    )
+}
